02c06eb5cfc8b9287228168c70363a47c1ac9b23
Pawankv89 <pawankv89@gmail.com>
2020-04-30 15:17:34 +0530

    Initial commit

--- Dynamic Row Height/ViewController.swift
+++ Dynamic Row Height/ViewController.swift
@@ -1,19 +1,10 @@
-# Dynamic-Row-Height
-
-## UITableView Dynamic Row Height
-
-
-Added Some screens here.
-
-![](https://github.com/pawankv89/Dynamic-Row-Height/blob/master/images/screen_1.png)
-![](https://github.com/pawankv89/Dynamic-Row-Height/blob/master/images/screen_2.png)
-
-
-## Usage
-
-#### Controller
-
-```swift
+//
+//  ViewController.swift
+//  Dynamic Row Height
+//
+//  Created by Pawan kumar on 30/04/20.
+//  Copyright © 2020 Pawan Kumar. All rights reserved.
+//
 
 import UIKit
 
@@ -103,24 +94,5 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
-
-
-
-
-```
-
-## Requirements
-
-### Build
-
-Xcode Version 11.3 (11C29), iOS 13.2.0 SDK
-
-## License
-
-This code is distributed under the terms and conditions of the [MIT license](LICENSE).
-
-## Change-log
-
-A brief summary of each this release can be found in the [CHANGELOG](CHANGELOG.mdown). 
 
 
